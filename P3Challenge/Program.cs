cf062bc569f4532c73d2c698d7217bc141102616
Therese Hume <Therese.Hume@atu.ie>
2025-03-13 15:44:24 +0000

    Add project files.

---
+++ P3Challenge/Program.cs
@@ -0,0 +1,132 @@
+﻿/*Worksheet 5 Part 3 : 
+  Author: Therese Hume
+  Date: Updated Feb 2025
+  Purpose: Translate a numeric Cheque to words*/
+using System.Security.Cryptography;
+using Microsoft.Win32.SafeHandles;
+
+namespace P3Challenge
+{
+ internal class Program
+ { 
+   static void Main(string[] args)
+   {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double amount = 0;
+
+            Console.WriteLine(ConvertCheque(0.50));
+            Console.WriteLine(ConvertCheque(3.00));
+            Console.WriteLine(ConvertCheque(5.20));
+            Console.WriteLine(ConvertCheque(20.50));
+            Console.WriteLine(ConvertCheque(28.50));
+            Console.WriteLine(ConvertCheque(350.00));
+            Console.WriteLine(ConvertCheque(4700.95));
+            Console.WriteLine(ConvertCheque(90000.50));
+            Console.WriteLine(ConvertCheque(123456789.50));
+
+
+
+            Console.Write("Please enter the amount of the cheque: ");
+
+            if (double.TryParse(Console.ReadLine(), out amount))
+                {
+                Console.WriteLine($"--------------------");
+                Console.WriteLine($"the amount of: {amount:c}");
+                Console.WriteLine($"({ConvertCheque(amount)})");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+            
+   }
+
+        /// <summary>
+        /// Converts an amount in euro to a text string, writing the number as words.
+        ///
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        static string ConvertCheque(double amount)
+            {
+                int euro = (int)amount;
+                int cents = (int)((amount - euro) * 100);
+                if (cents == 0)
+                {
+                    return ConvertNumberToText(euro) + " euro";
+                }
+                else if (euro==0)
+                {
+                return ConvertNumberToText(cents) + " cents";
+                }
+                else
+                {
+                    return ConvertNumberToText(euro) + " euro and " + ConvertNumberToText(cents) + " cents";
+                }
+            }
+
+            /// <summary>
+            /// Converts a given number to text.
+            /// </summary>
+            /// <param name="amount"></param>
+            /// <returns></returns>
+            static string ConvertNumberToText(long amount)
+            {
+
+                string numberInWords = "";
+                string[] units = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                string[] tens = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (amount < 0)
+                {
+                    return ("The number must be positive");
+                }
+                else if (amount == 0)
+                {
+                    numberInWords = "zero";
+                }
+
+                else if (amount < 20)
+                {
+                    numberInWords = units[amount];
+                }
+
+                else if (amount < 100)
+                {
+                    numberInWords = (tens[amount / 10] + " " + units[amount % 10]);
+                }
+                else if (amount < 1000)
+                {
+                    numberInWords = ConvertNumberToText(amount / 100) + " hundred ";
+
+                    if (amount % 100 > 0)
+                    {
+                        numberInWords = numberInWords + " and " + ConvertNumberToText(amount % 100);
+                    }
+                }
+                else if (amount < 1000000)
+                {
+                    numberInWords = ConvertNumberToText(amount / 1000) + " thousand";
+
+                    if (amount % 1000 > 0)
+                    {
+                        numberInWords = numberInWords + " , " + ConvertNumberToText(amount % 1000);
+                    }
+                }
+                else if (amount < 1000000000)
+                {
+
+                    numberInWords = ConvertNumberToText(amount / 1000000) + " million ";
+
+                    if (amount % 1000000000 > 0)
+                    {
+                        numberInWords = numberInWords + " , " + ConvertNumberToText(amount % 1000000);
+                    }
+                }
+                return numberInWords;
+            }
+
+ }
+}
+
---
+++ part 1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            string  text = " ababababcdcdcdcdcdcd";
+            Console.WriteLine(text.IndexOf('d'));
+            text = text.Replace('a', 'b');
+            Console.WriteLine(text);
+            Console.WriteLine(text.Replace('a', 'b').Replace('c', 'd'));
+        
+            string numberString = "1,2,3,4,5";
+            int sum = 0;
+            string[] numbersString = numberString.Split(',');
+            foreach (string s in numberString.Split(','))
+            {
+                sum += int.Parse(s);
+            }
+            Console.WriteLine(sum);
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            char[] letters = alphabet.ToCharArray();
+            int[] counts = CountOccurrences("abcdefghijkl", letters);
+            for (int i = 0; i < counts.Length; i++)
+               Console.WriteLine($"{alphabet[i]}  {counts[i]}");
+           
+        }
+       static  int[] CountOccurrences(string s, char[] letters) 
+        {
+            int[] count = new int[letters.Length];
+
+            for(int i=0;i<count.Length;i++)
+            {
+                if (s.Contains(letters[i]))
+                {
+                    count[i]++;
+                }
+            
+            }
+
+            return count;
+        }
+
+    }
+}
